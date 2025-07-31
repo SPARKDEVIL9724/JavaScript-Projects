ffd3c172008351a7cdf5f8f0620c71f85b669f9c
SPARKDEVIL9724 <rishi8240agarwal@gmail.com>
2025-07-31 15:24:30 +0530

    started github user finder project

---
+++ Github-User-FInder/script.js
@@ -0,0 +1,8 @@
+// getting dom elements
+const searchButton = document.querySelector('.search-button');
+const userDetails = document.querySelector('.user-details-card');
+const popularRepos = document.querySelector('.popular-repos');
+const displayUsername = document.querySelector('.user-name-display');
+
+// events
+searchButton.addEventListener('click', () => {});
